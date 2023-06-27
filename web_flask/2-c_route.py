9e4c463d9b4fe1012ec57aef28b92c08fb751768
jkarenzi <j.karenzi@alustudent.com>
2023-06-27 18:57:10 +0200

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-"""flask"""
+"""flask web application"""
 
-from flask import Flask, escape
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
+    """
+    Display 'Hello HBNB!'
+    """
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Display 'HBNB'
+    """
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    text = escape(text).replace('_', ' ')
-    return f'C {text}'
+    """
+    Display 'C ' followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    # Replace underscores with spaces
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
