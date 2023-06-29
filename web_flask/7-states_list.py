9ea106cb9dbf593cd7e82864c43bdc46761741d4
jkarenzi <j.karenzi@alustudent.com>
2023-06-29 11:56:27 +0200

    added remaining tasks

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Starts a web flask application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close storage"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """List all states"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
