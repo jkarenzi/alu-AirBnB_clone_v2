93c49e70760f73a1a865e249d3bd5d51620c2c25
jkarenzi <j.karenzi@alustudent.com>
2023-06-29 18:45:24 +0200

    added files

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""flask"""
-
+"""Start a Flask web application.
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,12 +8,15 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display a message"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Display a HBNB"""
     return 'HBNB'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""flask web application"""
+"""Start a Flask web application.
+The application listens on 0.0.0.0, port 5000
+"""
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """
-    Display 'Hello HBNB!'
-    """
+def hello_hbnb():
+    """Display a message"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Display 'HBNB'
-    """
+    """Display a HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """
-    Display 'C ' followed by the value of the text variable
-    (replace underscore _ symbols with a space)
-    """
-    # Replace underscores with spaces
+    """Display C followed by the value of the text variable"""
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
